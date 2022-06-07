ac1f88feaf39131d4f6c4e880c83c0c9aded16fa
Denys <denys.zinoviev@gmail.com>
2022-06-07 13:24:23 +0200

    Initial commit

---
+++ hw3/helper_functions.py
@@ -0,0 +1,39 @@
+def _sigma0(num: int):
+    """As defined in the specification."""
+    num = (_rotate_right(num, 7) ^
+           _rotate_right(num, 18) ^
+           (num >> 3))
+    return num
+
+def _sigma1(num: int):
+    """As defined in the specification."""
+    num = (_rotate_right(num, 17) ^
+           _rotate_right(num, 19) ^
+           (num >> 10))
+    return num
+
+def _capsigma0(num: int):
+    """As defined in the specification."""
+    num = (_rotate_right(num, 2) ^
+           _rotate_right(num, 13) ^
+           _rotate_right(num, 22))
+    return num
+
+def _capsigma1(num: int):
+    """As defined in the specification."""
+    num = (_rotate_right(num, 6) ^
+           _rotate_right(num, 11) ^
+           _rotate_right(num, 25))
+    return num
+
+def _ch(x: int, y: int, z: int):
+    """As defined in the specification."""
+    return (x & y) ^ (~x & z)
+
+def _maj(x: int, y: int, z: int):
+    """As defined in the specification."""
+    return (x & y) ^ (x & z) ^ (y & z)
+
+def _rotate_right(num: int, shift: int, size: int = 32):
+    """Rotate an integer right."""
+    return (num >> shift) | (num << size - shift)
---
+++ hw3/test.py
@@ -0,0 +1,15 @@
+from SHA256 import generate_hash
+
+if __name__ == '__main__':
+    TESTSET = [
+            {'val' : 'pokek doliloli', 'hash' : 'f34d6a11af126c4d27256deb127ad5609da7d6db49499bc99104e1daa66ce361'},
+            {'val' : 'DDDDDDDD', 'hash' : 'b1eedd29abf7e8b9b5c67030fc59dd42a4e16a0585c26176de357a4757515ed6'},
+            {'val' : 'kek hello world :)', 'hash' : '6c375e609a486ca75913d2ef85e36eaccef93c66bc38dc14de33ca97377897fd'}
+        ]
+
+
+
+    for test in TESTSET:
+        assert generate_hash(test['val']).hex() == test['hash']
+
+    print('All tests passed')
